5ec39de656bc73ab37e2cf9e8e6c3472eedb57e3
M. ABI RAFDI AL KHAIRI <abinakashima@gmail.com>
2025-10-23 21:24:04 +0800

    Update Tugas.php

---
+++ Tugas.php
@@ -0,0 +1,47 @@
+<?php
+echo "<h2>Tugas Deret PHP Dasar</h2>";
+
+echo "<h3>Deret (a)</h3>";
+$a1 = 4;
+$a2 = 6;
+$a3 = 9;
+$a4 = 13;
+$a5 = 18;
+
+echo "$a1 $a2 $a3 $a4 $a5 ";
+
+$a6 = $a5 + 6;
+$a7 = $a6 + 7;
+
+echo "$a6 $a7";
+echo "<br>";
+
+echo "<h3>Deret (b)</h3>";
+$b1 = 2;
+$b2 = 2;
+$b3 = 3;
+$b4 = 3;
+$b5 = 4;
+
+echo "$b1 $b2 $b3 $b4 $b5 ";
+
+$b6 = 4;
+$b7 = 5;
+
+echo "$b6 $b7";
+echo "<br>";
+
+echo "<h3>Deret (c)</h3>";
+$c1 = 1;
+$c2 = 9;
+$c3 = 2;
+$c4 = 10;
+$c5 = 3;
+
+echo "$c1 $c2 $c3 $c4 $c5 ";
+
+$c6 = 11;
+$c7 = 4;
+
+echo "$c6 $c7";
+?>
